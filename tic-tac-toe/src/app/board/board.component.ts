2b1d9d105d00b17297888e41d9a3c0b740a179f9
Mithc <isha23@yandex.ru>
2018-07-06 08:50:54 +0300

    logic ready

--- tic-tac-toe/src/app/board/board.component.ts
+++ tic-tac-toe/src/app/board/board.component.ts
@@ -64,12 +64,12 @@ import {LogicService} from "../logic.service";
   `]
 })
 export class BoardComponent implements OnInit {
-  public boardHeight: number;
-  public boardWidth: number;
+  private boardHeight: number;
+  private boardWidth: number;
   private player: string;
   public matrix: Array<Array<number | string>>;
   private win: string;
-  public status = "The game is on"
+  public status = "The game is on";
 
   constructor(private logicService: LogicService) {
     this.boardHeight = Math.floor((document.documentElement.clientWidth - 100) / 47) < 5 ? 5 : Math.floor((document.documentElement.clientWidth - 30) / 47);
@@ -84,9 +84,9 @@ export class BoardComponent implements OnInit {
     this.player = "player";
     if (this.matrix[row][col] === null && this.win === null) {
       this.matrix[row][col] = this.player;
-      let winner = this.chkWinner(this.matrix);
+      let winner = BoardComponent.chkWinner(this.matrix);
       this.matrix = this.logicService.computerPlay(this.matrix);
-      winner = this.chkWinner(this.matrix)
+      winner = BoardComponent.chkWinner(this.matrix);
       if (winner !== 0) {
         this.win = winner + " win!"
       }
@@ -94,45 +94,44 @@ export class BoardComponent implements OnInit {
   }
 
   public restart () {
-    this.matrix = []
+    this.matrix = [];
     for(let i = 0; i < this.boardWidth; i++){
       this.matrix[i] = [];
       for(let j = 0; j < this.boardHeight; j++){
         this.matrix[i][j] = null;
       }
     }
-
     this.win = null;
   }
 
-  private chkLine(a, b, c, d, e) {
+  private static chkLine(a, b, c, d, e) {
     return ((a != null) && (a == b) && (a == c) && (a == d) && (a == e));
   }
 
-  private chkWinner(bd) {
+  private static chkWinner(bd) {
     // Check down
     let r, c;
     for (r = 0; r < bd.length - 5; r++)
       for (c = 0; c < bd[0].length; c++)
-        if (this.chkLine(bd[r][c], bd[r + 1][c], bd[r + 2][c], bd[r + 3][c], bd[r + 4][c]))
+        if (BoardComponent.chkLine(bd[r][c], bd[r + 1][c], bd[r + 2][c], bd[r + 3][c], bd[r + 4][c]))
           return bd[r][c];
 
     // Check right
     for (r = 0; r < bd.length; r++)
       for (c = 0; c < bd[0].length - 4; c++)
-        if (this.chkLine(bd[r][c], bd[r][c + 1], bd[r][c + 2], bd[r][c + 3], bd[r][c + 4]))
+        if (BoardComponent.chkLine(bd[r][c], bd[r][c + 1], bd[r][c + 2], bd[r][c + 3], bd[r][c + 4]))
           return bd[r][c];
 
     // Check down-right
     for (r = 0; r < bd.length - 4; r++)
       for (c = 0; c < bd[0].length-4; c++)
-       if (this.chkLine(bd[r][c], bd[r + 1][c + 1], bd[r + 2][c + 2], bd[r + 3][c + 3], bd[r + 4][c + 4]))
+       if (BoardComponent.chkLine(bd[r][c], bd[r + 1][c + 1], bd[r + 2][c + 2], bd[r + 3][c + 3], bd[r + 4][c + 4]))
           return bd[r][c];
 
     // Check down-left
     for (r = 4; r < bd.length; r++)
       for (c = 0; c < bd[0].length - 4; c++)
-        if (this.chkLine(bd[r][c], bd[r - 1][c + 1], bd[r - 2][c + 2], bd[r - 3][c + 3], bd[r - 4][c + 4]))
+        if (BoardComponent.chkLine(bd[r][c], bd[r - 1][c + 1], bd[r - 2][c + 2], bd[r - 3][c + 3], bd[r - 4][c + 4]))
           return bd[r][c];
 
     return 0;
